c77a2872cc78d1a4104567422d07cbb16ee61a35
marinaspr <hyejuaya@gmail.com>
2025-06-20 10:32:11 -0300

    css, footer e routes atualizado

--- atividadePW/src/app/app-routing/app-routing.module.ts
+++ atividadePW/src/app/app-routing/app-routing.module.ts
@@ -2,14 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { HomeComponent } from '../home/home.component';
-import { FuncionalidadesComponent } from '../funcionalidades/funcionalidades.component';
 import { SobreComponent } from '../sobre/sobre.component';
+import { MetasComponent } from '../funcionalidades/funcionalidades.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'metas', component: FuncionalidadesComponent },
+  { path: 'metas', component: MetasComponent },
   { path: 'sobre', component: SobreComponent },
   { path: '**', redirectTo: 'login' }
 ];
--- atividadePW/src/app/footer/footer.component.ts
+++ atividadePW/src/app/footer/footer.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {}
+export class FooterComponent {
+  constructor(private router: Router) {}
+
+   metas(){
+    this.router.navigate(['/metas']);
+  }
+   sobre(){
+    this.router.navigate(['/sobre']);
+  }
+   home(){
+    this.router.navigate(['/home']);
+  }
+}
